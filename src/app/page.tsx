45210928afe463cd81a05d7cf9edc056a13af1fc
felipedestroo <fdestrocontact@gmail.com>
2024-10-16 17:14:15 -0300

    feat: add footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/navbar";
 import Content from "./components/home";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
     <><div className="absolute top-0 z-[-2] h-screen w-screen rotate-180 bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)]" />
-    <div>
+    <div className="container m-auto">
         <Navbar />
         <Content />
+        <Footer />
       </div>
     </>  
   );
