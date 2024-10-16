c67175ea73666f8867474a2363a9b534b62fc1c7
felipedestroo <fdestrocontact@gmail.com>
2024-10-16 17:15:05 -0300

    feat: change bg-color

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -4,7 +4,7 @@ import logo from "../../../public/logo.png"
  export default function Navbar() {
   return (
     <div>
-      <div className="flex flex-col bg-pink-500 w-full h-full justify-center items-center gap-4">
+      <div className="flex flex-col bg-blue-500 w-full h-full justify-center items-center gap-4">
         <Image src={logo} alt="Logo First" className="mt-2"/>
         <div>
           <ul className="flex gap-4 text-white text-center mb-2 font-bold flex-wrap justify-center ">
