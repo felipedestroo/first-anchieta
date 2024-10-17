4595f0f620ac098029546611f6d0efbb3cfae9f9
felipedestroo <fdestrocontact@gmail.com>
2024-10-17 17:12:51 -0300

    feat: removed h-auto / w-auto

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,5 +1,5 @@
 export default function Footer() {
   return (
-    <div className="bottom-0 bg-blue-500 w-full h-auto text-center">footer</div>
+    <div className="bottom-0 bg-blue-500 text-center">footer</div>
   )
 }
