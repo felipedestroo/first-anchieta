6bc7ea5ed02bc264feef9e80f0c6b957573c4e19
felipedestroo <fdestrocontact@gmail.com>
2024-10-21 16:34:59 -0300

    feat: update pages

--- src/app/components/footer.tsx
+++
@@ -1,5 +0,0 @@
-export default function Footer() {
-  return (
-    <div className="bottom-0 bg-blue-500 text-center">footer</div>
-  )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Navbar from "./components/navbar";
-import Content from "./components/home";
-import Footer from "./components/footer";
+import Navbar from "../components/navbar";
+import Content from "../components/home";
+import Footer from "../components/footer";
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
 
-      <div className="container mx-auto">
+      <div className="">
       <Navbar />
       <Content />
       <Footer />
