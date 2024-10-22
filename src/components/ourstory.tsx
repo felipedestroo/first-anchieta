1214a9321b95998e294c0300d75f1418c7a3d426
felipedestroo <fdestrocontact@gmail.com>
2024-10-22 14:33:34 -0300

    feat: start new section

---
+++ src/components/ourstory.tsx
@@ -0,0 +1,8 @@
+export default function Ourstory() {
+  return (
+    <div className="flex justify-center content-center">
+        <h1 className="text-blue-500 font-bold text-transform: uppercase">Nossa História</h1>
+    </div>
+  )
+}
+
