763532c6f6925e2ff77026385ea4ad2d54368fc2
felipedestroo <fdestrocontact@gmail.com>
2024-10-29 17:06:48 -0300

    feat: start ourstory section

--- src/components/ourstory.tsx
+++ src/components/ourstory.tsx
@@ -1,7 +1,21 @@
 export default function Ourstory() {
   return (
-    <div className="flex justify-center content-center">
-        <h1 className="text-blue-500 font-bold text-transform: uppercase">Nossa História</h1>
+    <div className="flex justify-center content-center mt-16 flex-col">
+        <h1 className="text-blue-500 font-bold text-transform: uppercase text-2xl">Nossa História</h1>
+
+        <div className="">
+          <h1>A Missão do #FIRSTLikeAGirl</h1>
+          <p>
+            O #FIRSTLikeAGirl se esforça para melhorar a cultura de STEM ao compartilhar as experiências das mulheres e meninas <br />
+            bem-sucedidas na FIRST. Ao destacar as incríveis mulheres da FIRST e suas histórias, as meninas podem encontrar modelos <br />
+            com os quais se identificam e se inspiram. Por meio desta campanha nas redes sociais, empoderamos as meninas com confiança <br />
+            para superar as pressões culturais, seguir seus sonhos em STEM e se tornarem membros ativos da comunidade FIRST. Melhoramo <br />
+            a cultura de STEM ao fornecer modelos que inspiram meninas a se tornarem a próxima geração de líderes em ciência e tecnolo <br />
+
+            Nossa missão é incentivar meninas e mulheres em todos os lugares a perceberem suas oportunidades, a participarem ativamente e <br />
+            prosperarem na comunidade FIRST e a retribuírem o incentivo que encontramos umas nas outras.
+          </p>
+        </div>
     </div>
   )
 }
